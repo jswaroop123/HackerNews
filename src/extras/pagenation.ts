9447d6c067624b8d9f152d5cb426bee29aba5fc7
Jyothi Swaroop N <jswaroop2003@gmail.com>
2025-05-10 12:48:25 +0530

    Implement pagination utility and remove Prisma client

--- src/extras/pagenation.ts
+++ src/extras/pagenation.ts
@@ -1,14 +1,10 @@
-//pagination.ts
 import type { Context } from "hono";
-
-// This is for getting the pagination parameters and
-// make sure they are within the page limits
 export const getPagination = (context: Context) => {
   const page = parseInt(context.req.query("page") || "1", 10);
   const limit = parseInt(context.req.query("limit") || "10", 10);
 
   const safePage = isNaN(page) || page < 1 ? 1 : page;
-  const safeLimit = isNaN(limit) || limit < 1 ? 10 : limit;
+  const safeLimit = isNaN(limit) || limit < 1 ? 3 : limit;
 
   return {
     page: safePage,
